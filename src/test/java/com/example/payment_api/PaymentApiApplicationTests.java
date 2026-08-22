c52790d2da9d2d4c229cb6155a33072efe6c7507
Luvuyo Mbewu <luvuyombewu@gmail.com>
2026-08-22 21:30:28 +0200

    Fix test import and disable security filters

--- src/test/java/com/example/payment_api/PaymentApiApplicationTests.java
+++ src/test/java/com/example/payment_api/PaymentApiApplicationTests.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc(addFilters = false)(addFilters = false)
 class PaymentApiApplicationTests {
 
     @Autowired
